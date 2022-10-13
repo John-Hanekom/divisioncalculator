5e4b192e49ed1a03f6bbab2823dea3902886093e
John Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-10-13 13:30:14 +0000

    Apparently, I didn't fix the code last time. Fixed this time, for sure.

--- main.py
+++ main.py
@@ -2,19 +2,14 @@
 # Date: October 13th, 2022
 # 
 # This program divides two inputted numbers through repeated subtraction.
-
-def main():
-	remainder = 0
-	calc = int(input('input the divident/numerator: '))
-	divident = int(input('input the divisor/denominator: '))
-	divisor = calc
-	while calc >= divident:
-		if calc >= divident:
-			calc -= divident
-			remainder += 1
-			print(divisor," divided by: ",divident," is equal to:")
-			print(remainder," with a remainder of: ",calc,".")
-	main()
-main()
-
-
+remainder = 0
+calc = int(input("input the divident/numerator: "))
+divident = int(input("input the divisor/denominator: "))
+divisor = calc
+while calc >= divident:
+	if calc >= divident:
+		calc -= divident
+		remainder += 1
+		print(divisor," divided by: ",divident," is equal to:")
+		print(remainder," with a remainder of: ",calc,".")
+	
