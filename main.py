fabb8d51e6656928d8e3eed95fefb7af48444a87
John Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-10-13 13:33:06 +0000

    Just realized it wasn't broken in the first place. Reverted the code and removed the pointless code

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 # Date: October 13th, 2022
 # 
 # This program divides two inputted numbers through repeated subtraction.
-remainder = 0
-calc = int(input("input the divident/numerator: "))
-divident = int(input("input the divisor/denominator: "))
-divisor = calc
-while calc >= divident:
-	calc -= divident
-	remainder += 1
-	print(divisor," divided by: ",divident," is equal to:")
-	print(remainder," with a remainder of: ",calc,".")
+
+def main():
+	remainder = 0
+	calc = int(input('input the divident/numerator: '))
+	divident = int(input('input the divisor/denominator: '))
+	divisor = calc
+	while calc >= divident:
+		calc -= divident
+		remainder += 1
+		print(divisor," divided by: ",divident," is equal to:")
+		print(remainder," with a remainder of: ",calc,".")
+	main()
+main()
