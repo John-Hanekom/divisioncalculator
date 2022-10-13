17d89c02026730ef390c45aa6f25a39ade3b9dbd
John Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-10-13 13:24:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Created by: John Hanekom
+# Date: October 13th, 2022
+# 
+# This program divides two inputted numbers through repeated subtraction.
+
+def main():
+	remainder = 0
+	calc = int(input('input the divident/numerator: '))
+	divident = int(input('input the divisor/denominator: '))
+	divisor = calc
+	while calc >= divident:
+		if calc >= divident:
+			calc -= divident
+			remainder += 1
+			print(divisor," divided by: ",divident," is equal to:")
+			print(remainder," with a remainder of: ",calc,".")
+	main()
+main()
+
+
